78fcbbaa6107a9006c4c50fa2bb243e54c6b672f
Prodipta Banerjee <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-03-12 15:46:16 +0600

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as htmlToImage from 'html-to-image';
+
+function toPDF(_png: string) {
+
+}
+
+const handleDivToImage = () => {
+    htmlToImage.toJpeg(document.getElementById('divToDownload') as HTMLElement, {quality: 0.95})
+        .then(function (blob) {
+            window.saveAs(blob, 'Image.png');
+            toPDF(blob);
+        });
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" id="divToDownload">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            
+            <body>
+
+            </body>
+            <footer>
+                <div>
+                    <button onClick={handleDivToImage}>Download Image</button>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
+
+
